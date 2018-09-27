3dc2520c8f6ffbf012947aa247d0c92275656361
CSTEAM\fabio.polato <fabio.polato@csteam.com>
2018-09-27 18:28:56 +0200

    Sviluppo interfaccia

--- src/app/crea-fattura-prodotti/crea-fattura-prodotti.component.ts
+++ src/app/crea-fattura-prodotti/crea-fattura-prodotti.component.ts
@@ -6,12 +6,36 @@ export interface Product {
   price: number;
 }
 
+export interface PrecompiledProduct {
+  description: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-crea-fattura-prodotti',
   templateUrl: './crea-fattura-prodotti.component.html',
   styleUrls: ['./crea-fattura-prodotti.component.css']
 })
 export class CreaFatturaProdottiComponent implements OnInit {
+
+  precompiledProducts: PrecompiledProduct[] = [
+    {
+      description: "Primo",
+      price: 9
+    },
+    {
+      description: "Primo + Contorno",
+      price: 10
+    },
+    {
+      description: "Secondo + Contorno",
+      price: 10
+    },
+    {
+      description: "Completo",
+      price: 12
+    }
+  ];
 
   products: Product[] = [
     {
